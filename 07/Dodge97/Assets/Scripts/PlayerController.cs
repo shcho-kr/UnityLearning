7d0914709e0995d9e500a19748848302fae23680
kyushu1996@gmail.com <103431041+shcho-kr@users.noreply.github.com>
2023-04-06 00:15:20 +0900

    dodge done

--- 07/Dodge97/Assets/Scripts/PlayerController.cs
+++ 07/Dodge97/Assets/Scripts/PlayerController.cs
@@ -25,5 +25,8 @@ public class PlayerController : MonoBehaviour
 
     public void Die() {
         gameObject.SetActive(false);
+
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManager.EndGame();
     }
 }
